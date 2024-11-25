673092fe3574e6fb87a4427f5994b131b13f004d
josefortyfive <jose.fortyfive@gmail.com>
2024-11-25 13:57:36 +0800

    Added C Projects

---
+++ Training/Learning/C/03_starting_write_code/practice_01/src/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char **argv){
+
+    int myFavoriteNumber = 0;
+
+    printf("Please enter your favorite number\n");
+    scanf("%d", &myFavoriteNumber);
+    printf("You've enter the number %d\n", myFavoriteNumber);
+
+    return 0;
+}
---
+++ Training/Learning/C/Array/Project1/Project1/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+
+	int n; 
+
+	n = 1 + 1;
+
+	cout <<"\nThe answer is : "+ n;
+
+	return 0;
+}
